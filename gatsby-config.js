098e52874d0672fb8628db4bb9cbfa027e03e2d4
balta-z-r <zunigaruizb@gmail.com>
2020-10-26 01:01:48 +0800

    fix: remove template metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,25 +26,14 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `Balta's Magical Place`,
+        short_name: `Balta`,
+        description: `Hi, I'm Balta`,
         start_url: `/`,
         background_color: `#141821`,
         theme_color: `#f6ad55`,
         display: `standalone`,
-        icons: [
-          {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`
-          },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`
-          }
-        ]
+        icon: '/icon.jpg'
       }
     },
     `gatsby-plugin-offline`,
